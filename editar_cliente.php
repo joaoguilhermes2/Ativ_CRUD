1a9ccb1a7c5990a02f6b3ceeb01cd09e56d8d0ce
João Guilherme Ortigosa <dry_jg@hotmail.com>
2025-02-03 23:07:16 -0400

    Atualização Atividade Crud

--- editar_cliente.php
+++ editar_cliente.php
@@ -37,6 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Pages/Css/estilo_editar.css">
     <title>Editar Cliente</title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $clientes = $cliente->listar();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Pages/Css/estilo_index.css">
     <title>Lista de Clientes</title>
 </head>
 <body>
